df927b6cd9ebe69631af18fc152f6718949ac225
Taras Rushchak <taraskhust@gmail.com>
2018-04-06 12:56:57 +0300

    first_commit

---
+++ src/assets/app.js
@@ -0,0 +1,24 @@
+// require(`./css/utils/fonts.styl`)
+// require('./css/utils/vars.styl')
+// require('./css/utils/mixins.styl')
+require('../assets/css/style.styl');
+// require(`./css/includes/includes.styl`)
+// require(`./css/pages/index.styl`)
+// require(`./node_modules/normalize.css/normalize.css`)
+// require('./css/uikit-rtl.css')
+
+
+// require('./js/custom.js')
+// require('./js/slick.js')
+// require('./js/jquery-1.11.0.min.js')
+// require('./js/uikit.js')
+// require('./js/uikit-icons.js')
+// let secondFile = require('./second.js')
+// const json = require('./data.json');
+// console.log('Hello from app.js');
+
+
+// clickme.addEventListener('click',()=>{
+// var template = require('./included.jade');
+// container.innerHTML = template();
+// })
---
+++ webpack.config.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const glob = require('glob-all');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const PurifyCSSPlugin = require('purifycss-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module .exports = {
+    entry: {
+        app: './src/assets/app.js',
+        // contact: './src/contact.js',
+ },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, "dist"),
+        // publicPath: '/'
+    },
+    module: {
+    rules: [
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+    },
+    {
+        test: /\.jade$/,
+        use: 'jade-loader'
+    },
+    {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+            pretty:true
+        }
+    },
+    {
+        test: /\.(jpg|png|gif|jpeg|svg)$/,
+        use: [
+        'file-loader?name=images/[name].[ext]',
+        // 'file-loader?name=[name].[ext]&outputPath=../images/&publicPath=.images/',
+        'image-webpack-loader'
+        ]
+    },
+        // {
+        //     test: /\.svg$/,
+        //     loader: 'svg-sprite-loader',
+        //     options: {
+        //         runtimeCompat: true
+        //     }
+        // },
+        // {
+        // loader: 'svgo-loader',
+        //     options: {
+        //         plugins: [
+        //             {removeTitle: true},
+        //             {convertColors: {shorthex: false}},
+        //             {convertPathData: false}
+        //         ]
+        //     }
+        // },
+
+    // {   test: /\.(woff2?)$/, loader: 'url-loader?limit=10000' },
+    {   test: /\.(woff2?|ttf|eot|otf|svg)$/, loader: 'file-loader?name=fonts/[name].[ext]' },
+
+        {   test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader'],
+                publicPath: '../'
+            })
+        },
+    {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader','sass-loader'],
+            publicPath: '../'
+        })
+    },
+    {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader','postcss-loader','stylus-loader'],
+            publicPath: '../'
+        })
+    },
+  ]
+},
+
+
+// Плагины и НАстройки
+plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Index',
+    template: './src/assets/pug/pages/index.pug',
+    hash: true,
+    // excludeChunks: ['contact'],
+    // minify: {
+    //     collapseWhitespace: true
+    // },
+}),
+    new HtmlWebpackPlugin({
+    title: 'Photo',
+    template: './src/assets/pug/pages/photo.pug',
+    hash: true,
+    // chunks: ['contact'],
+    filename: 'photo.html',
+    }),
+
+    new HtmlWebpackPlugin({
+        title: 'Form',
+        template: './src/assets/pug/pages/form.pug',
+        hash: true,
+        // chunks: ['contact'],
+        filename: 'form.html',
+    }),
+
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+    }),
+
+    new ExtractTextPlugin({
+        filename: 'css/[name].css',
+        disable: false,
+        allChunks: true
+    }),
+    new PurifyCSSPlugin({
+        // paths: glob.sync(path.join(__dirname, 'src/*.pug')),
+        paths: glob.sync([
+            path.join(__dirname, 'src/assets/pug/*.pug'),
+            path.join(__dirname, 'src/assets/pug/includes/*.pug'),
+            path.join(__dirname, 'src/assets/pug/pages/*.pug'),
+            path.join(__dirname, 'src/assets/js/*.js')
+        ]),
+        purifyOptions: { info: true, minify: false }
+    }),
+]
+};
