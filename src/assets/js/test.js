b095f6dd65476c1d7d23441c8c42571ebf2bc0be
Taras Rushchak <taraskhust@gmail.com>
2018-07-08 11:50:13 +0300

    index html done

---
+++ src/assets/js/test.js
@@ -0,0 +1,7 @@
+var milkInGrams = 20;
+console.log(milkInGrams);
+milkInGrams = 40;
+console.log(milkInGrams);
+var firstNumber = 100 + 50 / 2;
+var secondNumber = (100 + 50) / 2;
+console.log(firstNumber + secondNumber - milkInGrams);
